b3563626070b0cf5eb7be29b80789f614481396a
Mathieu Brujan <mathieu.brujan@epitech.eu>
2023-01-28 20:34:09 +0100

    feat: folder structure for the component part of the project

---
+++ component/client/ComponentExample.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace RType {
+    namespace Client {
+        struct ComponentExample {
+            ComponentExample() {};
+        };
+    }
+}
---
+++ component/server/ComponentExample.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace RType {
+    namespace Server {
+        struct ComponentExample {
+            ComponentExample() {};
+        };
+    }
+}
