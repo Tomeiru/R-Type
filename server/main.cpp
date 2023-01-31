d5e14003eccc7b114e0046fbf806facb057cf736
Mathieu Brujan <mathieu.brujan@epitech.eu>
2023-01-31 19:15:48 +0100

    feat: creation of UDPHandler and call to it

--- server/main.cpp
+++ server/main.cpp
@@ -1,4 +1,44 @@
-int main(void)
+#include <iostream>
+
+#include "../ecs/RuntimeException.hpp"
+#include "../common/PackageManager.hpp"
+#include "../common/UDPHandler.hpp"
+#include "../ecs/Coordinator.hpp"
+
+std::uint16_t parseArguments(int ac, char **av) {
+    if (ac != 2)
+        throw std::invalid_argument("parseArguments: Wrong number of argument (2 arguments expected)");
+    std::uint64_t port = std::stoul(av[1]);
+    if (port > 65535)
+        throw std::invalid_argument("parseArguments: Port should be a number between 0 and 65535");
+    return (port);
+}
+
+int main(int ac, char **av)
 {
+    try {
+        std::uint16_t port = parseArguments(ac, av);
+        RType::Network::PackageManager package_manager;
+        RType::Network::UDPHandler handler(port, package_manager);
+        handler.startHandler();
+        handler.stopHandler();
+    }
+    catch(std::invalid_argument &e) {
+        std::cerr << "An exception appeared in the argument checking part of the code: " << e.what() << std::endl;
+        return (84);
+    }
+    catch (std::out_of_range &e) {
+        std::cerr << "An exception appeared in the argument checking part of the code: " << "parseArguments: Port should be a number between 0 and 65535" << std::endl;
+        return (84);
+    }
+    catch(ECS::RuntimeException &e) {
+        std::cerr << "An exception appeared in the ECS part of the code: " << e.what() << std::endl;
+        return (84);
+    }
+    catch (...) {
+        std::cerr << "An unknown exception appeared" << std::endl;
+        return (84);
+    }
+
     return (0);
 }
