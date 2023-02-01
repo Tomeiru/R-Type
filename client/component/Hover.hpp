622a23c17bd65799152b636cf1a2f1ae41d73d4b
ludovic-str <ludovic.sutter@epitech.eu>
2023-02-01 22:09:52 +0100

    Feat(documentation): base components documentation

--- client/component/Hover.hpp
+++ client/component/Hover.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
 namespace SFML {
+    /**
+     * @brief Component that allows an entity to be hovered
+     */
     struct Hover {
+        /**
+         * @brief Construct a new Hover Component
+         * 
+         * @param hovered Whether the entity is hovered or not
+         */
         Hover(bool hovered = false) : hovered(hovered) {};
         bool hovered;
     };
