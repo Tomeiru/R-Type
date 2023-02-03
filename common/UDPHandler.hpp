e8b4a2f4ef4a2f198af5a748c131f8457015f34a
Mathieu Brujan <mathieu.brujan@epitech.eu>
2023-02-03 21:25:51 +0100

    feat(doc): Doxygen comments for the UDPHandler

--- common/UDPHandler.hpp
+++ common/UDPHandler.hpp
@@ -11,9 +11,17 @@
 #include "ReceivedPacket.hpp"
 
 namespace RType::Network {
+    /**
+     * @brief
+     */
     class UDPHandler {
     public:
-        UDPHandler(std::uint16_t port, std::shared_ptr<Network::PackageManager> package_manager) : _port(port), _started(false), _package_manager(package_manager) {}
+        /**
+         * @brief Constructs a new UDPHandler object
+         * @param port The port to bind the handler to
+         * @param package_manager The package manager that will handle the packet registration and creation
+         */
+        UDPHandler(std::uint16_t port, std::shared_ptr<Network::PackageManager> &package_manager) : _port(port), _started(false), _package_manager(package_manager) {}
         void startHandler() {
             if (_started)
                 throw RuntimeException("Server::startHandler", "Handler already started");
@@ -23,6 +31,9 @@ namespace RType::Network {
             _started = true;
             _thread = std::thread([this] {while (_started) {receive();}});
         }
+        /**
+         * @brief Stops the handler
+         */
         void stopHandler() {
             if (!_started)
                 throw RuntimeException("Server::stopHandler", "Handler has not been started yet");
@@ -31,11 +42,19 @@ namespace RType::Network {
             _thread.join();
         }
 
+        /**
+         * @brief Registers a packet to the package manager
+         * @tparam PayloadT The type of the packet
+         */
         template<typename PayloadT>
         void registerPacket() {
             return _package_manager->registerPacket<PayloadT>();
         }
 
+        /**
+         * @brief Receives a packet from the network
+         * @return The status of the reception or the size received
+         */
         std::uint32_t receive() {
             Network::Header header{};
             SFML::IpAddress sender(0);
@@ -54,20 +73,41 @@ namespace RType::Network {
             return received;
         }
 
+        /**
+         * @brief Sends a packet to the network
+         * @tparam PayloadT The type of the packet
+         * @param payload The payload of the packet
+         * @param address The address to send the packet to
+         * @param port The port to send the packet to
+         */
         void send(const void *data, std::size_t size, const SFML::IpAddress &address, uint16_t port) {
             if (_socket.send(data, size, address, port) == SFML::UDPSocket::Done)
                 std::cout << "UDPHandler::send: data successfully sent" <<  std::endl;
         }
 
+        /**
+         * @brief Creates a packet
+         * @tparam PayloadT The type of the packet
+         * @param payload The payload of the packet
+         * @return The packet
+         */
         template<typename PayloadT>
         Network::Packet<PayloadT> createPacket(PayloadT &payload) {
             return _package_manager->createPacket(payload);
         }
 
+        /**
+         * @brief Checks if the queue is empty
+         * @return If the queue is empty
+         */
         bool isQueueEmpty() {
             return (_queue.empty());
         };
 
+        /**
+         * @brief Pops an element from the queue
+         * @return The element
+         */
         ReceivedPacket popElement() {
             return (_queue.pop());
         }
