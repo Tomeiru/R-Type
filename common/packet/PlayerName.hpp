95d111d85cf9b2c4d7b3834160edc7992c398b51
Mathieu Brujan <mathieu.brujan@epitech.eu>
2023-02-03 21:25:14 +0100

    feat(doc): Doxygen comments for all packets

--- common/packet/PlayerName.hpp
+++ common/packet/PlayerName.hpp
@@ -3,7 +3,14 @@
 #include <string>
 
 namespace RType::Packet {
+    /**
+     * @brief PlayerName is a class that represents a player name packet
+     */
     struct PlayerName {
+        /**
+         * @brief Constructs a new PlayerName object
+         * @param name The name of the player
+         */
         PlayerName(std::string name = "") {
             for (int i = 0; i < 15; i++) {
                 _name[i] = (i < name.size() ? name[i] : '\0');
