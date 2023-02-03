7a2295fee859b8067ba5c95863bbbdbd367426b7
Mathieu Brujan <mathieu.brujan@epitech.eu>
2023-02-03 20:28:58 +0100

    feat: handle receiving connection from players until four ot them connects then starts the game

--- server/main.cpp
+++ server/main.cpp
@@ -4,6 +4,9 @@
 #include "../common/PackageManager.hpp"
 #include "../common/UDPHandler.hpp"
 #include "../ecs/Coordinator.hpp"
+#include "../server/PlayerManager.hpp"
+#include "../common/packet/PlayerName.hpp"
+#include "../common/packet/GameStart.hpp"
 
 std::uint16_t parseArguments(int ac, char **av) {
     if (ac != 2)
@@ -14,15 +17,55 @@ std::uint16_t parseArguments(int ac, char **av) {
     return (port);
 }
 
+void registerResources(std::unique_ptr<ECS::Coordinator> &coordinator, std::uint16_t port) {
+    auto package_manager = coordinator->registerResource<RType::Network::PackageManager>();
+    coordinator->registerResource<RType::Network::UDPHandler>(port, package_manager);
+    coordinator->registerResource<RType::PlayerManager>();
+}
+
+void registerPackets(std::unique_ptr<ECS::Coordinator> &coordinator) {
+    auto package_manager = coordinator->getResource<RType::Network::PackageManager>();
+
+    package_manager->registerPacket<RType::Packet::PlayerName>();
+    package_manager->registerPacket<RType::Packet::GameStart>();
+}
+
+void waiting_for_players(std::unique_ptr<ECS::Coordinator> &coordinator) {
+    auto package_manager = coordinator->getResource<RType::Network::PackageManager>();
+    auto udp_handler = coordinator->getResource<RType::Network::UDPHandler>();
+    auto player_manager = coordinator->getResource<RType::PlayerManager>();
+
+    std::cout << "Waiting for 4 players to start the game!" << std::endl;
+    while (player_manager->getNbPlayerConnected() < 4) {
+        while(!udp_handler->isQueueEmpty()) {
+            RType::Network::ReceivedPacket packet_received = udp_handler->popElement();
+            std::shared_ptr<RType::Network::Header> header = package_manager->decodeHeader(packet_received.packet_data);
+            if (!header || header->id != package_manager->getTypeId<RType::Packet::PlayerName>())
+                continue;
+            auto decoded_player_name = package_manager->decodeContent<RType::Packet::PlayerName>(packet_received.packet_data);
+            player_manager->addNewPlayer(decoded_player_name->_name, packet_received.sender);
+            std::cout << "A new player joined the game! We're now " << player_manager->getNbPlayerConnected() << std::endl;
+        }
+    }
+    RType::Packet::GameStart game_start;
+    auto packet = package_manager->createPacket<RType::Packet::GameStart>(game_start);
+    std::cout << "Everyone joined! The game can finally start!" << std::endl;
+    player_manager->sendPacketToAllPlayer(&packet, sizeof(packet), udp_handler);
+}
+
 int main(int ac, char **av)
 {
     try {
         std::uint16_t port = parseArguments(ac, av);
-        ECS::Coordinator coordinator;
-        auto package_manager = coordinator.registerResource<RType::Network::PackageManager>();
-        auto udp_handler = coordinator.registerResource<RType::Network::UDPHandler>(port, package_manager);
-        udp_handler->startHandler();
+        auto coordinator = std::make_unique<ECS::Coordinator>();
 
+        registerResources(coordinator, port);
+        registerPackets(coordinator);
+
+        auto udp_handler = coordinator->getResource<RType::Network::UDPHandler>();
+
+        udp_handler->startHandler();
+        waiting_for_players(coordinator);
         udp_handler->stopHandler();
     }
     catch(std::invalid_argument &e) {
@@ -37,10 +80,10 @@ int main(int ac, char **av)
         std::cerr << "An exception appeared in the ECS part of the code: " << e.what() << std::endl;
         return (84);
     }
-    catch (...) {
+    /*catch (...) {
         std::cerr << "An unknown exception appeared" << std::endl;
         return (84);
-    }
+    }*/
 
     return (0);
 }
