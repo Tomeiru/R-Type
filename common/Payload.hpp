8499b763c566ee72e792fa0361decaabd24519df
ludovic-str <ludovic.sutter@epitech.eu>
2023-02-03 21:33:40 +0100

    feat-enemy-player-movements-payloads

---
+++ common/Payload.hpp
@@ -0,0 +1,16 @@
+struct ClientPlayerPosition {
+    short x;
+    short y;
+};
+
+struct ServerPlayerPosition {
+    short spriteId;
+    short x;
+    short y;
+};
+
+struct ServerEnemyPosition {
+    short spriteId;
+    short x;
+    short y;
+};
