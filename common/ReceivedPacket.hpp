bcd4c82455264d35bf431def9b908aee9b9bc7c3
Mathieu Brujan <mathieu.brujan@epitech.eu>
2023-02-03 21:27:30 +0100

    feat(doc): Doxygen comments for the ReceivedPacket

--- common/ReceivedPacket.hpp
+++ common/ReceivedPacket.hpp
@@ -4,8 +4,23 @@
 #include "UDPClient.hpp"
 
 namespace RType::Network {
+    /**
+     * @brief ReceivedPacket is a class that represents a received packet
+     */
     struct ReceivedPacket {
+        /**
+         * @brief Constructs a new ReceivedPacket object
+         * @param ip The IP address of the sender
+         * @param port The port of the sender
+         * @param packet_data The data of the packet
+         */
         ReceivedPacket(const SFML::IpAddress &ip, std::uint16_t port, const std::string &packet_data) : sender(ip, port), packet_data(packet_data) {}
+
+        /**
+         * @brief Constructs a new ReceivedPacket object
+         * @param sender The sender of the packet
+         * @param packet_data The data of the packet
+         */
         ReceivedPacket(const UDPClient &sender, const std::string &packet_data) : sender(sender), packet_data(packet_data) {}
         UDPClient sender;
         std::string packet_data;
